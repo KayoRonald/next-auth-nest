c0318bed45b66290e4dd0c7c3f9bf26280b3f84f
Kayo Ronald <kayoronald4321@gmail.com>
2024-05-30 13:05:15 +0000

    feat: Add Header and footer

--- app/src/layout/Layout.tsx
+++ app/src/layout/Layout.tsx
@@ -1,18 +1,23 @@
-import { Box } from '@chakra-ui/react';
-import Header from './Header';
-import { LayoutProps } from '@/types';
+import { Box } from "@chakra-ui/react";
+import Header from "./Header";
+import { LayoutProps } from "@/types";
+import FooterChakra from "./Footer";
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <Box transition="0.5s ease-out" minHeight="100vh">
-      <Box margin="0 auto" maxWidth={1300}>
-        <Box margin="8">
-          <Box as="main" marginY={22}>
-            {children}
+    <>
+      <Header />
+      <Box transition="0.5s ease-out" minHeight="100vh">
+        <Box margin="0 auto" maxWidth={1300}>
+          <Box margin="8">
+            <Box as="main" marginY={22}>
+              {children}
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+      <FooterChakra/>
+    </>
   );
 };
 
