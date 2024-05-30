0c14408b6bcce129f4a628556b689bbde66e8638
Kayo Ronald <kayoronald4321@gmail.com>
2024-05-30 13:03:47 +0000

    feat: Add types for authentication context and credentials

--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -2,4 +2,19 @@ import type { ReactNode } from 'react';
 
 export type LayoutProps = {
   children: ReactNode;
-};
+}
+
+export type ISignUpCredentials = {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+}
+
+export type ISignInCredentials = Pick<ISignUpCredentials, 'email' | 'password'>;
+
+export type IAuthContext = {
+  signIn(credentials: ISignInCredentials): Promise<void>;
+  signUp(credentials: ISignUpCredentials): Promise<void>;
+  signOut(): void;
+}
